017282c7530ae977df5bec3f56984bd085fcc1a6
Hitesh <hk9137636@gmail.com>
2020-06-23 10:29:50 +0530

    5 Pages Slided.

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import UserRegistration from './pages/UserRegistration.js';
 import EmailUserRegistration from './pages/EmailUserRegistration.js';
 import LoadScreenConsumer from './pages/LoadScreenConsumer.js';
 import LocationPermission from './pages/LocationPermission.js';
-
+import HomeGettingStarted from './pages/HomeGettingStarted.js';
 function App() {
   return (
     <Router>
       <div>
-        <Route exact path="/" component = {UserRegistration} />
+        <Route exact path="/user_registration" component = {UserRegistration} />
         <Route exact path="/email_user_registration" component = {EmailUserRegistration} />
         <Route exact path="/load_screen_consumer" component = {LoadScreenConsumer} />
         <Route exact path="/location_permission" component = {LocationPermission} />
+        <Route exact path="/home_getting_started" component = {HomeGettingStarted} />
       </div>
     </Router>
   );
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.render(
   <React.StrictMode>
